65d00e2604ff2cc7330be785ab0cce5830c4cf2a
yltrcc <ttxxly@gmail.com>
2022-04-20 12:20:10 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,22 @@
+import { defineHopeConfig } from "vuepress-theme-hope";
+import themeConfig from "./themeConfig";
+
+export default defineHopeConfig({
+  lang: "zh-CN",
+  title: "主题演示",
+  description: "vuepress-theme-hope 的演示",
+
+  base: "/",
+
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
+
+  themeConfig,
+});
